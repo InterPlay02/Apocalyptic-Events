f71581565ff53aa0f27a119831e8e800950ac43d
InterPlay <Rodrigointerplay@hotmail.com>
2024-01-28 12:30:38 -0300

    Initial commit

---
+++ src/main/java/interplay/apocalypticevents/ApocalypticEvents.java
@@ -0,0 +1,60 @@
+package interplay.apocalypticevents;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ApocalypticEvents.MODID)
+public class ApocalypticEvents
+{
+    public static final String MODID = "apocalypticevents";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ApocalypticEvents() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){
+
+    }
+
+    // Add block items to the mod's tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event){
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){
+
+        }
+    }
+}
---
+++ src/main/java/interplay/apocalypticevents/Config.java
@@ -0,0 +1,18 @@
+package interplay.apocalypticevents;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.IConfigSpec;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Forge's config APIs
+@Mod.EventBusSubscriber(modid = ApocalypticEvents.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event) {
+
+    }
+}
